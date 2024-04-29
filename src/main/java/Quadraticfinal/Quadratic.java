8ab4023d449970f634feac7d45b177e131b37429
syntaxroror <louisvalencia308@yahoo.com>
2024-04-29 11:41:29 +0800

    Quadratic finals with user input

---
+++ src/main/java/Quadraticfinal/Quadratic.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Quadraticfinal;
+
+
+/**
+ *
+ * @author percy
+ */
+import java.util.ArrayList;
+public class Quadratic {
+	private int a;
+	private int b;
+	private int c;
+	private int x;
+    
+	public Quadratic(int a,int b, int c){
+    	this.a = a;
+    	this.b = b;
+    	this.c = c;
+    	//this.x = x;
+	}
+    
+        @Override
+    public String toString() {
+        return a + "x^2 + " + b + "x + " + c;
+    }
+
+    
+	public Quadratic() {
+	}
+
+public int setA(int ay){
+this.a = ay;
+return this.a;
+}
+
+public int setB(int by){
+	this.b = by;
+	return this.b;
+}
+
+public int setC(int cy){
+	this.c = cy;
+	return this.c;
+}
+
+public int setX(int xy){
+	this.x = xy;
+	return this.x;
+}
+
+public int getA(){
+	return this.a;
+}
+
+public int getB(){
+	return this.b;
+}
+
+public int getC(){
+	return this.c;
+}
+
+public int getX(){
+	return this.x;
+}
+
+public int evaluate(){
+	int result = (a*x*x) + (b*x) + c;
+	return result;
+	///3,5,1
+}
+
+public int getDiscriminant(){
+	int disc = (b*b)-(4*a*c);
+   return disc;
+}
+
+public void isImaginary(){
+	if (getDiscriminant()<0){
+    	System.out.println("The roots are imaginary");
+	}else{
+	System.out.println("The roots are not imaginary");
+}
+}
+public double getFirstRoot(){
+   double result = (int)(-b + java.lang.Math.sqrt(getDiscriminant())) / (2*a);
+   return result;
+}
+
+public double getSecondRoot(){
+	double result = (int) (-b - java.lang.Math.sqrt(getDiscriminant())) / (2*a);
+	return result;
+}
+
+public void isPerfectSquare(){
+	if(getDiscriminant()==0){
+    	System.out.println("The equation is a perfect square\n");
+	}else{
+    	System.out.println("The equation is not a perfect square\n");
+	}
+}
+
+public Quadratic add(Quadratic otherAdd,Quadratic otherAdd2){
+  Quadratic result = new Quadratic();
+  result.a = otherAdd.a + otherAdd2.a;
+  result.b = otherAdd.b + otherAdd2.b;
+  result.c = otherAdd.c + otherAdd.c;
+  return result;
+}
+
+public void displayQuadratic(){
+	System.out.println("The result of two quadratic equations added together is: ");
+   System.out.println(this.a + "x^2 " +"+ " +  this.b + "x + " + this.c + " = 0");
+   
+}
+}
+//java.lang.Math.sqrt
+
+
+/*  displayQuadratic()
+    add(Quadratic otherAdd)
+    isPerfectSquare()
+    getSecondRoot()
+    getFirstRoot()
+    isImaginary()
+    getDiscriminant()
+    evaluate()
+
+*/
---
+++ src/main/java/Quadraticfinal/QuadraticTester.java
@@ -0,0 +1,250 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Quadraticfinal;
+
+/**
+ *
+ * @author percy
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class QuadraticTester {
+	public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+    	int a;
+    	int b;
+    	int c;
+    	int x;
+    	int a2;
+    	int b2;
+    	int c2;
+    	int x2;
+        int input;
+        int input2;
+    	boolean hi=true;
+        ArrayList<Quadratic> quads = new ArrayList<Quadratic>();
+    	while(hi==true){
+   	boolean yes =true;
+    	//boolean answer = true;
+   	 //while(answer){
+         
+         /*  displayQuadratic()
+    add(Quadratic otherAdd)
+    isPerfectSquare()
+    getSecondRoot()
+    getFirstRoot()
+    isImaginary()
+    getDiscriminant()
+    evaluate()
+
+*/
+         int answer;
+         System.out.println("1. Create quadratic equation ");
+         System.out.println("2. Add two quadratic equations together ");
+         System.out.println("3. Is it a perfect square?");
+         System.out.println("4. Get value of x^1 and x^2");
+         System.out.println("5. Evaluate the equation");
+         System.out.println("6. Exit");
+         answer = scanner.nextInt();
+         switch (answer){
+             case 1:
+                 while(true){
+                        System.out.println("Enter the coefficients of the quadratic equation (a, b, c)");
+                try{
+                        a = scanner.nextInt();
+                        b = scanner.nextInt();
+                        c = scanner.nextInt();
+                       
+                        Quadratic quad = new Quadratic(a,b,c);
+                        quads.add(quad);
+                        System.out.println("Equation is now added to the list.\n");
+                        break;                  
+                }catch(Exception e){
+                           System.out.println("Error occured while entering coefficients.\n");
+                           scanner.nextLine();       	 
+                    }
+                 }
+                  break;
+                  
+              case 2: 
+                 while(true){
+                     try{
+                     System.out.println("Select two quadratic equations to add together:");
+                     for (int i = 0; i<quads.size(); i++){
+                         System.out.println(i +" " + quads.get(i));
+                     }
+                    System.out.println("1st Quadratic equation: ");
+                    input = scanner.nextInt();
+                    System.out.println("2nd Quadratic equation:");
+                    input2 = scanner.nextInt();
+                    Quadratic quad1 = quads.get(input);
+                    Quadratic quad2 = quads.get(input2);
+                    Quadratic result = new Quadratic();
+                    System.out.println("Result: "+ result.add(quad1, quad2));
+                     }catch(Exception e){
+                         System.out.println("Error occured while adding two equations together\n");
+                     }
+                    break;
+                 }
+              case 3: 
+                  while (true){ 
+                      try{
+                 System.out.println("Select a quadratic equation to check: ");
+                 for(int i = 0; i<quads.size(); i++){
+                     System.out.println(i + " " + quads.get(i));
+                 }
+                 System.out.println("\n");
+                  input = scanner.nextInt();
+                  quads.get(input).isPerfectSquare();
+                  break;
+            	}catch (Exception e){
+                    System.out.println("Error finding the perfect square");
+                }
+             }
+                  break;
+              case 4: 
+                  while (true){
+                      System.out.println("Select a quadratic equation to enter value of x: ");
+                      for (int i = 0; i<quads.size(); i++){
+                          System.out.println(quads.get(i));
+                      }
+                      input = scanner.nextInt();
+                      System.out.println("\nEnter value of x: ");
+                      input2 = scanner.nextInt();
+                      Quadratic forthex = quads.get(input);
+                      forthex.setX(input2);
+                  }
+         }
+        }
+        }
+}
+             
+             /*
+             case 2: add()
+                     break;
+            
+                     
+             case 3: 
+         }
+         //}
+         }
+        }
+   	 /*
+    	while(true){
+    	System.out.println("Enter the coefficients of the quadratic equation (a, b, c):");
+    	try{
+     	a = scanner.nextInt();
+     	b = scanner.nextInt();
+     	c = scanner.nextInt();
+     	break;
+    	}catch(Exception e){
+        	System.out.println("Error occured while entering coefficients.\n");
+        	scanner.nextLine();       	 
+        	}
+   	}
+        /*
+    	while(true){
+    	System.out.println("Enter the value of x:");
+    	try{
+         	x = scanner.nextInt();
+         	break;
+    	}catch(Exception e){
+        	System.out.println("Error occured while entering value of x.\n");
+        	scanner.nextLine();
+      	 
+    	}
+   	}
+    	Quadratic quad = new Quadratic(a, b, c, x);
+   	 
+    	//EVALUATION
+    	System.out.println("Evaluating");
+    	System.out.println(quad.evaluate());
+    	System.out.println();
+   	 
+    	//GET DISCRIMINANT
+	System.out.println("Getting Discriminant");
+    	System.out.println(quad.getDiscriminant());
+    	System.out.println();
+   	 
+    	System.out.println("Is it imaginary?");
+    	quad.isImaginary();
+    	System.out.println();
+   	 
+    	System.out.println("Get first root");
+    	System.out.println(quad.getFirstRoot());
+    	System.out.println();
+   	 
+    	System.out.println("Get second root");
+    	System.out.println(quad.getSecondRoot());
+    	System.out.println();
+   	 
+    	System.out.println("Is it a perfect square");
+    	quad.isPerfectSquare();
+    	System.out.println();
+   	 
+    	System.out.println("Adding two quadractic equations together");
+   	 
+    	while(true){
+         	System.out.println("Enter coefficients for the second quadratic equation: (a,b,c)");
+      	try{
+     	a2 = scanner.nextInt();
+     	b2 = scanner.nextInt();
+     	c2 = scanner.nextInt();
+     	break;
+    	}catch(Exception e){
+        	System.out.println("Error occured while entering coefficients.\n");
+        	scanner.nextLine();
+      	 
+        	}
+    	}
+   	 
+    	while(true){
+    	System.out.println("Enter value of x: ");
+    	try{
+     	x2 = scanner.nextInt();
+     	break;
+    	}catch (Exception e){
+        	System.out.println("Error occured while entering value of x.\n");
+        	scanner.nextLine();
+       	 
+     	}
+    	}
+    	Quadratic otherQuad = new Quadratic(a2, b2, c2, x2);
+    	Quadratic result = quad.add(otherQuad);
+    	result.displayQuadratic();
+    	scanner.nextLine();
+           	 
+           	 
+    	while(true){
+                 	System.out.println("Would you like to enter another set of quadratic equation?: Y/N");
+         	try{
+    	answer = scanner.next();
+    	if(answer.equalsIgnoreCase("Y")){
+        	hi = true;
+        	break;
+    	}else if(answer.equalsIgnoreCase("N")){
+        	hi = false;
+        	break;
+        	}else{
+        	System.out.println("Please enter 'Y' or 'N' \n");
+        	scanner.nextLine();
+    	}
+ 	}
+    	catch(Exception e){
+        	System.out.println("Error occured while entering response.\n");
+        	}
+     	}
+    	}  
+        */
+	//}
+        
+
+
+
+
+/*for(int i = 0; i<quads.size(); i++){
+                         System.out.println(quads.get(i));
+                         }*/
